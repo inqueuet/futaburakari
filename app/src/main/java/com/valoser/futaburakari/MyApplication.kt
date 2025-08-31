f3f8d141a44a5fbeca74f4ac896f8083d635db69
admin@inqueuet.com <admin@inqueuet.com>
2025-08-31 19:42:00 +0900

    Add GIF animation support

--- app/src/main/java/com/valoser/futaburakari/MyApplication.kt
+++ app/src/main/java/com/valoser/futaburakari/MyApplication.kt
@@ -5,8 +5,11 @@ import android.content.pm.ApplicationInfo
 import android.util.Log
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
+import android.os.Build
 import coil.ImageLoader
 import coil.ImageLoaderFactory
+import coil.decode.GifDecoder
+import coil.decode.ImageDecoderDecoder
 import coil.decode.VideoFrameDecoder
 import dagger.hilt.android.HiltAndroidApp
 import kotlinx.coroutines.CoroutineScope
@@ -62,6 +65,13 @@ class MyApplication : Application(), Configuration.Provider, ImageLoaderFactory 
     override fun newImageLoader(): ImageLoader {
         return ImageLoader.Builder(this)
             .components {
+                // Enable animated GIF decoding
+                if (Build.VERSION.SDK_INT >= 28) {
+                    add(ImageDecoderDecoder.Factory())
+                } else {
+                    add(GifDecoder.Factory())
+                }
+                // Enable extracting a representative frame for videos
                 add(VideoFrameDecoder.Factory())
             }
             .build()
