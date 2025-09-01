253f42e6fa92351e2adfb579b130827676c6a15a
admin@inqueuet.com <admin@inqueuet.com>
2025-09-01 10:26:00 +0900

    バージョン更新

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,8 +27,8 @@ android {
         applicationId = "com.valoser.futaburakari"
         minSdk = 24
         targetSdk = 36
-        versionCode = 10
-        versionName = "2.1"
+        versionCode = 11
+        versionName = "2.2"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
