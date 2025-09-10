2dec9024f164a281aa8d71dabe660297dbb39900
admin@inqueuet.com <admin@inqueuet.com>
2025-09-10 19:43:09 +0900

    バージョン5.1にアップデート

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "com.valoser.futaburakari"
         minSdk = 24
         targetSdk = 36
-        versionCode = 40
-        versionName = "5.0"
+        versionCode = 41
+        versionName = "5.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
