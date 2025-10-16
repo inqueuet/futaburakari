b00a540c49229e9516534e36519aa8a2981bbf4d
admin@inqueuet.com <admin@inqueuet.com>
2025-10-16 19:29:54 +0900

    内部バージョンアップ

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
         applicationId = "com.valoser.futaburakari"
         minSdk = 24
         targetSdk = 36 // Android API 36
-        versionCode = 116 // 内部バージョン（Play Console 配信管理で使用）
+        versionCode = 117 // 内部バージョン（Play Console 配信管理で使用）
         versionName = "1.3" // 表示バージョン
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
